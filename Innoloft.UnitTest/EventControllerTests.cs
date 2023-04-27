cd3ad87149da1c7f7398b1725ad2ad16d06f9ac9
PETRATRUST\kwame.boateng <kwame.agyenim-boateng@petraonline.com>
2023-04-27 11:11:02 +0000

    remove comment

--- Innoloft.UnitTest/EventControllerTests.cs
+++ Innoloft.UnitTest/EventControllerTests.cs
@@ -31,7 +31,6 @@ namespace Innoloft.UnitTest
 
             //act
             var result = await controller.GetEventCreators();
-            Console.WriteLine(result);
 
             //assert
             Assert.NotNull(result);
